99b9daafeec62d30f7ca06fbdee47a8d3d484db7
Sarthak <sarthakdixit630@gmail.com>
2025-07-24 12:07:26 +0530

    AppointmentContext created and EntityFramework added

--- server/server/Data/AppointmentContext.cs
+++ server/server/Data/AppointmentContext.cs
@@ -1,6 +1,13 @@
-﻿namespace server.Data
+﻿using server.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace AppointmentBooking.Data
 {
-    public class AppointmentContext
+    public class AppointmentContext : DbContext
     {
+        public AppointmentContext(DbContextOptions<AppointmentContext> options) : base(options) { }
+
+        public DbSet<Appointment> Appointments => Set<Appointment>();
     }
 }
--- server/server/Models/Appointment.cs
+++ server/server/Models/Appointment.cs
@@ -1,6 +1,26 @@
-﻿namespace server.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
 {
     public class Appointment
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Patient name is required")]
+        public string PatientName { get; set; }
+
+        [Required(ErrorMessage = "Scheduled date is required")]
+        public DateOnly ScheduledDate { get; set; }
+
+        [Required(ErrorMessage = "Slot is required")]
+        public string Slot { get; set; }
+
+        [Required(ErrorMessage = "Doctor ID is required")]
+        public int DoctorId { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
     }
 }
